6670bc398cf30e780b4b368077918d54772921ac
seyed sina hosseini vaez <122821690+SinaHosseini@users.noreply.github.com>
2023-03-17 23:52:11 +0330

    Add files via upload

--- class media.py
+++ class media.py
@@ -21,7 +21,47 @@ one = pyfiglet.figlet_format("1")
 print(one)
 time.sleep(1)
 
-movie = Media("man of steel", "zack snyder", "7.1",
-              "https://youtu.be/EVsFbdjlpAE", "2013", "henry cavill")
-movie.showinfo()
-movie.download()
+# movie = Media("man of steel", "zack snyder", "7.1",
+#               "https://youtu.be/EVsFbdjlpAE", "2013", "henry cavill")
+# movie.showinfo()
+# movie.download()
+movie = []
+film = []
+series = []
+documentary = []
+clip = []
+
+
+def read_data():
+    f = open("database.txt", "r")
+
+    for line in f:
+        result = line.split(",")
+        if line == 0:
+            my_obj = Film(result[1], result[2], result[3],
+                          result[4], result[5], result[6])
+            film.append(my_obj)
+            movie.append(film)
+
+        elif line == 1:
+            my_obj = Series(result[1], result[2], result[3],
+                          result[4], result[5], result[6])
+            series.append(my_obj)
+            movie.append(series)
+
+        elif line == 2:
+            my_obj = Documentary(result[1], result[2], result[3],
+                          result[4], result[5], result[6])
+            documentary.append(my_obj)
+            movie.append(documentary)
+
+        elif line == 3:
+            my_obj = Clip(result[1], result[2], result[3],
+                          result[4], result[5], result[6])
+            clip.append(my_obj)
+            movie.append()
+
+    f.close()
+
+read_data()
+print(movie)
